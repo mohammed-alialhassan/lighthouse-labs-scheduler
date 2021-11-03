358ea57325c61e3072b701aa092f654bbf622ce6
Ali Al-Hassan <kenzo@Alis-Air.cogeco.local>
2021-11-03 01:33:46 -0400

    promise.all function set up in application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,9 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from 'components/Appointment'
 import "components/Appointment";
+import { getAppointmentsForDay } from "helpers/selectors";
 
+/*
 const appointments = [
   {
     id: 1,
@@ -62,33 +64,33 @@ const appointments = [
     id: 6,
     time: "5pm",
   },
-];
+]; */
 
 export default function Application(props) {
-
-  //const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([])
-  const setDay = day => setState(state => ({ ...state, day }));
 
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
   });
 
+  const setDay = (day) => setState({ ...state, day });
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
   useEffect(() => {
-       axios.get("http://localhost:8001/api/days")
-       .then(resp => {
-          console.log(resp)
-          setDays(() => resp.data)
-          setState(prev => ({ ...prev, days }));
-          setState(state => ({...state, days: resp.data}))
-         setDays(resp.data)
-       })
-       .catch(err => {
-         console.error(err)
-       })
-     }, []);
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
+    ]).then((all) => {
+      setState((prev) => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      }));
+    });
+  }, []);
 
   return (
     <main className="layout">
@@ -113,7 +115,10 @@ export default function Application(props) {
 />
       </section>
       <section className="schedule">
-      {appointments.map((appointment) => (<Appointment key={appointment.id} {...appointment} />))}
+      {dailyAppointments.map((appointment) => (
+      <Appointment key={appointment.id} 
+      {...appointment} 
+      />))}
       <Appointment key="last" time="5pm" />
       </section>
     </main>
--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,14 +2,14 @@ import React from "react"
 import DayListItem from "components/DayListItem"
 
 export default function DayList(props) {
-  const days = props.days.map(day => (
+  const list = props.days.map(day => (
     <DayListItem
       key={day.id}
       name={day.name}
       spots={day.spots}
       selected={day.name === props.day}
-      setDay={props.name}
+      setDay={() => props.setDay(day.name)}
     />
   ));
-  return <ul>{days}</ul>;
+  return <ul>{list}</ul>;
 }
